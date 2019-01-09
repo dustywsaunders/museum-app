fe9355099e47cffdf3a8d2886784b3b01f45ec76
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-09 15:48:41 +0100

    added conditinals and error reporting to app

--- detail-page.js
+++ detail-page.js
@@ -1,14 +1,39 @@
 
+// PASS VALIDATION
+function doesNotPassAllValidations(name, msg) {
+    // ERROR CONDITIONS
+    if (!msg && !name) {
+        alert('Whoops, you forgot to fill in your name and message!')
+      return true;
+    };
+
+    if (msg && !name) {
+        alert('Whoops, you forgot to fill in your name!')
+        return true;
+    };
+
+    if (!msg && name) {
+        alert('Whoops, you forgot to fill in your message!')
+        return true;
+    };
+  
+    if(msg.length > 280) {
+      alert('Whoops, your comment is too long!')
+      return true
+    };
+  
+    return false
+};
+
+
 // add event handler
 function submitComment() {
-
+    
     // gather data
     const inputField = document.getElementById('name')
     const name = inputField.value
-    console.log(name)
     const textArea = document.getElementById('msg')
     const msg = textArea.value
-    console.log(msg)
 
     // create the elements you need
     const comment = document.createElement('section')
@@ -17,18 +42,20 @@ function submitComment() {
 
     // adjust the elements we created
     h3.innerHTML = `${name} said:`
-    p.innerHTML = msg
+    p.innerHTML = '\"' + msg + '\"'
     comment.classList.add('comments')
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log(comment)
     
+    if (doesNotPassAllValidations(name, msg)) {
+        return null
+    } else {
     // displaying comments
     const commentSection = document.getElementById('comments')    
     commentSection.appendChild(comment)
-
+        
     // reset form values
     inputField.value = null
     textArea.value = null
-
+    }
 };
