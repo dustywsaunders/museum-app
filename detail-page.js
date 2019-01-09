ab635c08baa2691b15dcbc968432c780ddb7a99e
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-09 12:34:04 +0100

    lunch break

--- detail-page.js
+++ detail-page.js
@@ -1,3 +1,11 @@
 function submitComment() {
-    console.log("Hallo");
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    console.log(name)
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    console.log(msg)
+    const comment = document.createElement('section')
+    const heading = document.createElement('h3')
+    const para = document.createElement('p')
 };
